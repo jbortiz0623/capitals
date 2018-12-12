daaf27cd61c59539c3335ac416b946fe0955a0af
jbortiz0623 <jbortiz0623@gmail.com>
2018-12-11 18:10:45 -0800

    first lab

--- capitals.py
+++ capitals.py
@@ -151,3 +151,70 @@ states = [
     "name": "Wyoming",
     "capital": "Cheyenne"
 }]
+
+# `print`
+# - `input`
+# - `for loop`
+# - `sorted`
+# - `random.shuffle()`
+import random
+
+random.shuffle(states)
+
+print('Welcome to the State Game!')
+print('If it is correct you will get a point. If you are incorrect you will lose a point.')
+
+def state_game ():
+    wrong = 0
+    correct = 0
+    score = correct - wrong
+
+    i = 0
+
+    while i < len(states):
+        print ('What is the capital of '+ states[i]['name'] )
+        answer = input()
+        if answer == states[i]['capital']:
+            correct = correct + 1
+            print("Awesome you got it!")
+            print(f"You have answered {correct} correctly and {wrong} incorrectly.")
+            print(f"Total scrore is {score}")
+        else:
+            wrong = wrong + 1
+            print("Aww boo. That is incorrect.")
+            print(f"You have answered {correct} correctly and {wrong} incorrectly.")
+            print(f"Total scrore is {score}")
+        i = i + 1
+
+    print(f'What a game! Your final score was {score} out of 50')
+
+
+state_game()
+
+print('Would you like to play again?')
+answer = input()
+
+if answer == 'y' or 'yes' :
+    state_game()
+else:
+    print('Goodbye')
+    exit()```
+
+
+   while i < len(states):
+        print ('What is the capital of '+ states[i]['name'] )
+        answer = input()
+        if answer == states[i]['capital']:
+            correct = correct + 1
+            score = correct - wrong
+            print("Awesome you got it!")
+            print(f"You have answered {correct} correctly and {wrong} incorrectly.")
+            print(f"Total scrore is {score}")
+
+        else:
+            wrong = wrong + 1
+            score = correct - wrong
+            print("Aww boo. That is incorrect.")
+            print(f"You have answered {correct} correctly and {wrong} incorrectly.")
+            print(f"Total scrore is {score}")
+        i = i + 1
